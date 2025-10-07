c26fd78619756279ced3ef1e401605e5565e1e05
Ernesto Alves <98620218+Ernesto-Alves67@users.noreply.github.com>
2025-10-07 20:43:26 +0000

    checkpoint

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -16,7 +16,7 @@ const TopBar = styled.header`
   display: flex;
   align-items: center;
   justify-content: center;
-  gap: ${theme.spacing.sm};
+  gap: ${theme.spacing.lg};
 `;
 
 const Logo = styled.div`
@@ -25,22 +25,24 @@ const Logo = styled.div`
   // background-color: ${theme.colors.ufcatGreen};
   border-radius: ${theme.borderRadius.md};
   display: flex;
-  align-items: center;
+  //align-items: center;
   justify-content: center;
+  align-self: flex-end;
   color: white;
   font-weight: ${theme.fontWeight.bold};
   font-size: ${theme.fontSize.xl};
     img {
-    width: 70%; /* ou 100% se quiser preencher tudo */
+    width: auto; /* ou 100% se quiser preencher tudo */
     height: auto;
   };
 `;
 
 const Title = styled.h1`
-  font-size: ${theme.fontSize.xl};
+  font-size: ${theme.fontSize.xxxl};
   font-weight: ${theme.fontWeight.bold};
   color: ${theme.colors.textPrimary};
-  margin: 0;
+  align-self: flex-end;
+
 `;
 
 const MainContent = styled.main`
@@ -114,15 +116,6 @@ const screens = [
       </svg>
     )
   },
-  // { 
-  //   route: '/sigaa', 
-  //   label: 'SIGAA',
-  //   icon: (
-  //     <svg viewBox="0 0 24 24" fill="currentColor">
-  //       <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-1 17.93c-3.95-.49-7-3.85-7-7.93 0-.62.08-1.21.21-1.79L9 15v1c0 1.1.9 2 2 2v1.93zm6.9-2.54c-.26-.81-1-1.39-1.9-1.39h-1v-3c0-.55-.45-1-1-1H8v-2h2c.55 0 1-.45 1-1V7h2c1.1 0 2-.9 2-2v-.41c2.93 1.19 5 4.06 5 7.41 0 2.08-.8 3.97-2.1 5.39z"/>
-  //     </svg>
-  //   )
-  // },
 ];
 
 function Layout() {
